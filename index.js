eb88bdcef278dde35db4b14f63a0c9f60aa88a78
Erwyna Soo <erwynasoo@gmail.com>
2024-05-01 01:11:16 +0800

    Tester Website UI Design with Figma Conversion

---
+++ index.js
@@ -0,0 +1,87 @@
+import React from "react"
+import "./styles.css"
+
+export default function MaskGroup() {
+  return (
+    <div className="mask-group">
+      <div className="macbook-pro-161 clip-contents">
+        <div className="inpit-box">
+          <p className="input-box-text">Chat with Chatbot...</p>
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/tsrsd11vd0s-48%3A261?alt=media&token=ad530191-7c36-4fc5-a93b-bb58f6239f48"
+            alt="Not Found"
+            className="vector"
+          />
+        </div>
+        <div className="group-366">
+          <div className="group-552">
+            <p className="main-chatbot-header">Chatbot</p>
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/tsrsd11vd0s-48%3A278?alt=media&token=e21014ca-7488-462c-be79-6d5694d70b0a"
+              alt="Not Found"
+              className="sidebar-menu-button"
+            />
+          </div>
+          <div className="group-837">
+            <div className="chatbot-message-header">
+              <div className="group-793">
+                <div className="chatbot-icon-backround" />
+                <p className="chatbot-header">Chatbot</p>
+              </div>
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/tsrsd11vd0s-48%3A267?alt=media&token=84549bf8-debf-4620-9d11-0ad55085894e"
+                alt="Not Found"
+                className="chatbot-icon"
+              />
+            </div>
+            <div className="group-241">
+              <p className="chatbot-message">Hey there!</p>
+              <p className="user-message">
+                Hi, could you help me write an essay on a topic of choice?
+              </p>
+              <div className="group-544">
+                <div className="group-169">
+                  <img
+                    src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/tsrsd11vd0s-48%3A270?alt=media&token=13320b4f-47cc-4b30-b3df-2d4548b290a1"
+                    alt="Not Found"
+                    className="bot-profile-logo"
+                  />
+                  <p className="chatbot-header-2">Chatbot</p>
+                </div>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/tsrsd11vd0s-48%3A279?alt=media&token=61723923-1b60-41fc-b011-57a60f610532"
+                  alt="Not Found"
+                  className="loading-blob"
+                />
+              </div>
+              <div className="user-message-header">
+                <div className="group-461">
+                  <img
+                    src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/tsrsd11vd0s-48%3A275?alt=media&token=6a83675e-3053-4d37-abcb-3c45d2676c88"
+                    alt="Not Found"
+                    className="user-icon-backround-shape"
+                  />
+                  <p className="user-name">User</p>
+                </div>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/tsrsd11vd0s-48%3A277?alt=media&token=95c8caaa-8ba1-416f-bb79-d0cc9505912c"
+                  alt="Not Found"
+                  className="user-icon"
+                />
+              </div>
+            </div>
+          </div>
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/tsrsd11vd0s-48%3A283?alt=media&token=1f675662-fe95-40b2-851a-8c67a85b7b1c"
+            alt="Not Found"
+            className="sidebar-menu-button-1"
+          />
+        </div>
+        <p className="chatbot-header-3">
+          Chatbot may display inaccurate info, including about people, so
+          double-check its responses. Your privacy & Chatbot Apps
+        </p>
+      </div>
+    </div>
+  )
+}
