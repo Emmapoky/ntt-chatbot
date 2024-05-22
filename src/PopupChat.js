905373c695834b36a1a4887e5478e4b7d6af14a0
Erwyna Soo <erwynasoo@gmail.com>
2024-05-22 23:47:32 +0800

    Adjusted Popup

--- src/PopupChat.js
+++ src/PopupChat.js
@@ -1,9 +1,10 @@
-import React, { useState, useRef, useEffect } from 'react';
-import ChatIcon from '@mui/icons-material/Chat'; // Import the ChatIcon
+import React, { useEffect, useRef, useState } from 'react';
+import { marked } from 'marked';
 import SendIcon from '@mui/icons-material/ArrowCircleRightRounded';
+import ChatIcon from '@mui/icons-material/Chat'; // Import the ChatIcon
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import TypingIndicator from './TypingIndicator'; // Ensure you have the correct import for TypingIndicator
 import './PopupChat.css';
+import TypingIndicator from './TypingIndicator'; // Ensure you have the correct import for TypingIndicator
 
 const PopupChat = ({ handleUserMessage, messages, isChatbotTyping }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -34,63 +35,71 @@ const PopupChat = ({ handleUserMessage, messages, isChatbotTyping }) => {
     }
   };
 
-  return (
-    <div>
-      <div className="popup-chat-button-wrapper">
-        <div className="popup-chat-button-background">
-          <ChatIcon className="popup-chat-button" onClick={toggleChat} />
+  const getFormattedText = (text) => {
+    const rawMarkup = marked(text);
+    return { __html: rawMarkup };
+ };
+
+return (
+  <div>
+    <div className="popup-chat-button-wrapper">
+      <div className="popup-chat-button-background">
+        <ChatIcon className="popup-chat-button" onClick={toggleChat} />
+      </div>
+    </div>
+    {isOpen && (
+      <div className="popup-chat-popup">
+        <div className="popup-form-container">
+          <div className="popup-chat-header">
+            <div className="popup-profile-icon"></div>
+            <div className="popup-profile-info">
+              <div className="popup-profile-name">Your name</div>
+              <div className="popup-profile-status">Status</div>
+            </div>
+            <div className="popup-chat-settings">
+              <KeyboardArrowDownIcon />
+            </div>
+          </div>
+          <div className="popup-message-list">
+            {messages.map((msg, index) => (
+              <div key={index} className={`popup-message ${msg.sender}`}>
+                <div className="popup-message-header">
+                  <div className={`popup-profile-icon ${msg.sender}`}></div>
+                  <div
+                    className="popup-message-text"
+                    dangerouslySetInnerHTML={getFormattedText(msg.message)}
+                  ></div>
+                </div>
+              </div>
+            ))}
+            {isChatbotTyping && (
+              <div className="popup-typing-indicator-container">
+                <div className="popup-loader-message-header">
+                  <div className="popup-profile-icon popup-loader-profile-icon"></div>
+                  <div className="popup-loader-message-text">Chatbot is typing...</div>
+                </div>
+                <TypingIndicator />
+              </div>
+            )}
+            <div ref={messagesEndRef} />
+          </div>
+          <form className="popup-input-container" onSubmit={handleSendMessage}>
+            <input
+              type="text"
+              className="popup-input-field"
+              placeholder="input text"
+              value={inputValue}
+              onChange={handleInputChange}
+            />
+            <button type="submit" className="popup-send-button">
+              <SendIcon />
+            </button>
+          </form>
         </div>
       </div>
-      {isOpen && (
-        <div className="popup-chat-popup">
-          <div className="popup-form-container">
-            <div className="popup-chat-header">
-              <div className="popup-profile-icon"></div>
-              <div className="popup-profile-info">
-                <div className="popup-profile-name">Your name</div>
-                <div className="popup-profile-status">Status</div>
-              </div>
-              <div className="popup-chat-settings">
-                <KeyboardArrowDownIcon />
-              </div>
-            </div>
-            <div className="popup-message-list">
-              {messages.map((msg, index) => (
-                <div key={index} className={`popup-message ${msg.sender}`}>
-                  <div className="popup-message-header">
-                    <div className={`popup-profile-icon ${msg.sender}`}></div>
-                    <div className="popup-message-text">{msg.message}</div>
-                  </div>
-                </div>
-              ))}
-              {isChatbotTyping && (
-                <div className="popup-typing-indicator-container">
-                  <div className="popup-loader-message-header">
-                    <div className="popup-profile-icon popup-loader-profile-icon"></div>
-                    <div className="popup-loader-message-text">Chatbot is typing...</div>
-                  </div>
-                  <TypingIndicator />
-                </div>
-              )}
-              <div ref={messagesEndRef} />
-            </div>
-            <form className="popup-input-container" onSubmit={handleSendMessage}>
-              <input
-                type="text"
-                className="popup-input-field"
-                placeholder="input text"
-                value={inputValue}
-                onChange={handleInputChange}
-              />
-              <button type="submit" className="popup-send-button">
-                <SendIcon />
-              </button>
-            </form>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+    )}
+  </div>
+);
 };
 
 export default PopupChat;
