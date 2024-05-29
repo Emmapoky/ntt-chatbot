e433f142df15b5ceb8fead8021c349838764271e
Erwyna Soo <erwynasoo@gmail.com>
2024-05-29 20:37:44 +0800

    popup adjust

--- src/TypingIndicator.js
+++ src/TypingIndicator.js
@@ -1,11 +1,11 @@
 import './TypingIndicator.css';
 import React from 'react';
 
-const TypingIndicator = () => {
+const TypingIndicator = ({ variant }) => {
   return (
-    <div className="typing-indicator-container">
+    <div className={`typing-indicator-container ${variant}`}>
       <div className="loader-message-header">
-        <div className="loader-profile-icon"></div>
+        <div className={`loader-profile-icon ${variant}`}></div>
         <div className="profile-name">Gemini</div>
       </div>
       <div className="loader-container">
