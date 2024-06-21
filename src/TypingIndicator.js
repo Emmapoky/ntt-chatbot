8989bf93d76bcddcb183d1ca04dad95522b79052
Erwyna Soo <erwynasoo@gmail.com>
2024-06-22 00:37:36 +0800

    Popup Adjustments + Loading needed

--- src/TypingIndicator.js
+++ src/TypingIndicator.js
@@ -4,13 +4,16 @@ import React from 'react';
 const TypingIndicator = ({ variant }) => {
   return (
     <div className={`typing-indicator-container ${variant}`}>
-      <div className="loader-message-header">
+      <div className={`loader-message-header ${variant}`}>
         <div className={`loader-profile-icon ${variant}`}></div>
         {variant !== 'popup' && <div className="profile-name">Gemini</div>}
+        {variant === 'popup' && <div className="loader popup"></div>}
       </div>
-      <div className="loader-container">
-        <div className="loader"></div>
-      </div>
+      {variant !== 'popup' && (
+        <div className="loader-container">
+          <div className="loader"></div>
+        </div>
+      )}
     </div>
   );
 };
